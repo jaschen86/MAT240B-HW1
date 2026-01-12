c4da07591c3e4612ed584a1a40cb212a2cf4624c
Jason Chen <jhxchen7@outlook.com>
2026-01-12 01:34:09 -0800

    add square.cpp and cmake target

---
+++ square.cpp
@@ -0,0 +1,26 @@
+#include "everything.h"
+
+#define INIT_FREQUENCY 440.0
+#define TIME_SEC 2
+
+double square(double initial_frequency, long sample_index) {
+    double sum = 0.0;
+
+    long N = SAMPLE_RATE /(2 * initial_frequency);
+    double t = sample_index / SAMPLE_RATE;
+
+    for (int n = 1; n <= N; n+=2) {
+        double phase = 2.0 * n * pi * t * initial_frequency;
+        sum += sin(phase) / n;
+    }
+    return sum * 4 / pi;
+}
+
+int main() {
+
+    for (long t = 0; t < TIME_SEC * SAMPLE_RATE; t++) {
+        mono(square(INIT_FREQUENCY, t));
+    }
+
+    return 0;
+}
